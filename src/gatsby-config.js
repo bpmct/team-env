8906ab62681c6152caf9de8925631f830cf6fc6e
Ben <me@bpmct.net>
2021-11-19 19:17:16 +0000

    revert

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Ben Starter Blog v2`,
+    title: `Gatsby Starter Blog`,
     author: {
-      name: `Ben Potter`,
-      summary: `who lives and works in Austin building semi-useful things. 🚀 `,
+      name: `Jon Doe`,
+      summary: `who lives and works in San Francisco building useful things`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
